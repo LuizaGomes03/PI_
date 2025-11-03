9e600990b1e24bd7467b5ed9536f59411c6317bc
arthurromanin <arthur.romanin123@gmail.com>
2025-11-03 19:15:23 -0300

    update(colaboradores):  permite adicionar colaborador
    verificar usuario/email

--- back_end/routes/colaboradores.js
+++ back_end/routes/colaboradores.js
@@ -1,8 +1,10 @@
 import express from "express";
 const router = express.Router();
 import {
-    listarColaboradoresPorUnidade
+    listarColaboradoresPorUnidade,
+    criarColaborador
 } from "../controllers/colaboradoresController.js";
 
+router.post("/colaboradores", criarColaborador)
 router.get("/colaboradores/unidade/:unidadeId", listarColaboradoresPorUnidade);
 export default router;
