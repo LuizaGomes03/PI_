84af74118b1cdc6c143ef22c95cfb08c5b453fca
arthurromanin <arthur.romanin123@gmail.com>
2025-11-11 23:47:30 -0300

    feat: implementa escala compartilhada com banco de dados

--- back_end/routes/colaboradores.js
+++ back_end/routes/colaboradores.js
@@ -3,10 +3,12 @@ const router = express.Router();
 import {
   listarColaboradoresPorUnidade,
   criarColaborador,
+  listarEscalaCompartilhada,
   handleLogin
 } from "../controllers/colaboradoresController.js";
 
 router.get("/colaboradores/unidade/:unidadeId", listarColaboradoresPorUnidade);
+router.get("/escala/unidade/:unidadeId", listarEscalaCompartilhada);
 router.post("/colaboradores", criarColaborador);
 router.post("/login", handleLogin);
 
